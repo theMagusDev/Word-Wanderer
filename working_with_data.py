6216d7d3ee5a03e17ca7621c1672b90da69de455
Yuriy Magus <114507764+theMagusDev@users.noreply.github.com>
2023-10-20 17:46:13 +0300

    Added working_with_data.py file

---
+++ working_with_data.py
@@ -0,0 +1,13 @@
+def get_dictionary():
+    dictionary = dict()
+    return dictionary
+
+def save_dictionary(dictionary):
+    return 0
+
+def get_settings():
+    settings = dict()
+    return settings
+
+def save_settings():
+    return 0
